f695ae13a6b2950dc2850c10687d60f1f95d1dbc
Miguel Palhas <mpalhas@gmail.com>
2023-11-29 19:14:32 +0000

    code review

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,25 @@
-use std::sync::Arc;
-
-use ethers::signers::HDPath;
-use ethers::types::Address;
-use tokio::sync::Mutex;
+use ethers::signers::{HDPath, Ledger, LedgerError};
 
 #[tokio::main]
 async fn main() {
     // first call, works
+    // if ledger is disconnected we get Err(UnexpectedNullResponse) as expected
     let _ = dbg!(call_ledger().await);
 
-    let m = Arc::new(Mutex::new(()));
-    let handles = (0..3).map(|i| {
-        let m = m.clone();
+    // ISSUE 1
+    // second call works if ledger is connected,
+    // but panics if its disconnected
+    // comment this to reach 2nd issue
+    let _ = dbg!(call_ledger().await);
 
-        tokio::spawn(async move {
-            // even with a mutex to ensure only one call at a time, this poisons the HID mutex
-            let guard = m.lock().await;
-            println!("start {}", i);
-            let _ = call_ledger().await;
-            println!("end {}", i);
-        })
-    });
-
-    for handle in handles {
-        let _ = handle.await;
-    }
+    // ISSUE 2
+    // these fail with a poisoned mutex,
+    // regardless of ledger being connected or not
+    let _ = tokio::spawn(call_ledger()).await;
+    let _ = tokio::spawn(call_ledger()).await;
 }
 
-pub(crate) async fn call_ledger() -> anyhow::Result<Address> {
+pub(crate) async fn call_ledger() -> Result<Ledger, LedgerError> {
     let path: String = "m/44'/60'/0'/0/0".into();
-    let ledger = ethers::signers::Ledger::new(HDPath::Other(path), 1).await?;
-
-    Ok(ledger.get_address().await?)
+    Ledger::new(HDPath::Other(path), 1).await
 }
