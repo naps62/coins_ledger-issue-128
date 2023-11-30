75c7d4a0407d361538f9c4ee09fec8f168313d93
Miguel Palhas <mpalhas@gmail.com>
2023-11-30 07:15:03 +0000

    code review

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,12 @@ use ethers::signers::{HDPath, Ledger, LedgerError};
 #[tokio::main]
 async fn main() {
     // first call, works
-    // if ledger is disconnected we get Err(UnexpectedNullResponse) as expected
     let _ = dbg!(call_ledger().await);
 
-    // ISSUE 1
-    // second call works if ledger is connected,
-    // but panics if its disconnected
-    // comment this to reach 2nd issue
-    // let _ = dbg!(call_ledger().await);
+    // tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
 
-    // ISSUE 2
-    // these fail, first with Hid(InitializationError), then with a poisoned mutex
-    // regardless of ledger being connected or not
-    let _ = tokio::spawn(call_ledger()).await;
-    let _ = tokio::spawn(call_ledger()).await;
+    // second call, if done without sleep first, fails with `hid_error is not implemented yet`
+    let _ = dbg!(call_ledger().await);
 }
 
 pub(crate) async fn call_ledger() -> Result<Ledger, LedgerError> {
